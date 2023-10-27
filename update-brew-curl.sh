83daf26720ddeb3cecb155c05c51729792d5aac9
trewchainz <relytetan@gmail.com>
2023-10-27 11:25:57 -0700

    Update update-brew-curl.sh
    better commenting

--- update-brew-curl.sh
+++ update-brew-curl.sh
@@ -5,21 +5,19 @@ checkCurl=$( /usr/bin/su - "$currentUser" -c '/opt/homebrew/bin/brew list curl' 
 
 #run if user is logged in, exit if not
 if [[ $currentUser != "loginwindow" ]]; then
-    #command line tools for Xcode is a dependency of brew
-    echo "Checking Command Line Tools for Xcode"
-    #install command line tools if not present
-    xcode-select -p &> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "Command Line Tools for Xcode not found. Installing from softwareupdate…"
-        # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
-        touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-        PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
-        softwareupdate -i "$PROD" --verbose;
-    else
-        echo "Command Line Tools for Xcode have been installed."
-    fi
-    #if $checkCurl returns non-empty, install curl
+    #if $checkCurl returns non-empty, update curl
     if [[ ! -z $checkCurl ]]; then
+        #command line tools for Xcode is a dependency of brew
+        echo "Checking Command Line Tools for Xcode"
+        #install command line tools if not present
+        xcode-select -p &> /dev/null
+        if [ $? -ne 0 ]; then
+            echo "command line tools for Xcode not found. installing from softwareupdate..."
+            # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+            touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+            PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
+            softwareupdate -i "$PROD" --verbose;
+        fi
         echo "curl is installed via brew, updating..."
         #install new version of curl
         /usr/bin/su - "$currentUser" -c '/opt/homebrew/bin/brew install curl'
